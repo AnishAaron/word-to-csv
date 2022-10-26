d8a326b1dc5ca059fc25b31abfdb34502c1ca928
AnishAaron <75377703+AnishAaron@users.noreply.github.com>
2022-10-26 18:00:05 +0530

    File selector window added

--- src/App.java
+++ src/App.java
@@ -1,39 +1,48 @@
 import java.io.*;
 import java.util.*;
 import java.util.List;
+
+import javax.swing.JFileChooser;
+
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.apache.poi.xwpf.usermodel.XWPFParagraph;
 import com.opencsv.CSVWriter;
 
 public class App {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException, IOException {
         File csvfile = new File("src\\csvfile.csv");
-        try {
-            FileWriter outputfile = new FileWriter(csvfile);
-            CSVWriter writer = new CSVWriter(outputfile);
-            List<String[]> data = new ArrayList<String[]>();
 
-            FileInputStream docfile = new FileInputStream("src\\test.docx");
-            XWPFDocument docx = new XWPFDocument(docfile);
-            List<XWPFParagraph> para = docx.getParagraphs();
+        JFileChooser window = new JFileChooser();
+        int returnValue = window.showOpenDialog(null);
 
-            for(XWPFParagraph paragraph: para ){
-                System.out.println(paragraph.getText());
-                String[] sep_data = paragraph.getText().split(" ");
-                data.add(sep_data);
+        if(returnValue == JFileChooser.APPROVE_OPTION){
+            XWPFDocument docx = new XWPFDocument(new FileInputStream(window.getSelectedFile()));
+            
+            try {
+                FileWriter outputfile = new FileWriter(csvfile);
+                CSVWriter writer = new CSVWriter(outputfile);
+                List<String[]> data = new ArrayList<String[]>();
+
+                
+                List<XWPFParagraph> para = docx.getParagraphs();
+
+                for(XWPFParagraph paragraph: para ){
+                    System.out.println(paragraph.getText());
+                    String[] sep_data = paragraph.getText().split(" ");
+                    data.add(sep_data);
+                }
+
+                writer.writeAll(data);
+
+                docx.close();
+                writer.close();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-
-            writer.writeAll(data);
-
-            docx.close();
-            writer.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
         }
-
 
     }
 }
